c99c55192f08e6a21718a3fed7be3f69b0ac5211
Tieuphong1211 <146963159+Tieuphong1211@users.noreply.github.com>
2024-02-28 15:01:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var Addborder = $('.Addborder');
+    $('Addborder').click(function(){
+      $('Addborder').removeClass("borderProduct");
+      $(this).addClass("borderProduct");
+  });
+});
+
